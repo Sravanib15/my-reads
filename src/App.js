a87bac1c95bf5f57ae0db8ef3f10dcb3e18829f8
sravani.boggarapu <sravani.boggarapu@servicenow.com>
2018-01-06 20:41:27 -0800

    refactor: Refactored the code

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ class BooksApp extends React.Component {
   updateShelf = (book, shelf) => {
     BooksAPI.update(book, shelf);
 
-    this.setState(state => ({
-      books: state.books.filter(b => b.id !== book.id).concat(book)
-    }))
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books });
+    })
   }
 
   componentDidMount() {
@@ -28,39 +28,42 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        <Route exact path="/search" render={() => (
+        <Route exact path = "/search" render = {({ history }) => (
           <SearchBook
-            searchQuery={this.searchQuery}
-            updateShelf={this.updateShelf}
+            searchQuery = {this.searchQuery}
+            updateShelf = {(book, shelf) => {
+              this.updateShelf(book, shelf)
+              history.push('/');
+            }}
           />
         )}/>
-        <Route exact path="/" render={() => (
-          <div className="list-books">
-            <div className="list-books-title">
+        <Route exact path = "/" render={() => (
+          <div className = "list-books">
+            <div className = "list-books-title">
               <h1>MyReads</h1>
             </div>
 
-            <div className="list-books-content">
+            <div className = "list-books-content">
               <div>
                 <ListBooks
-                  shelfTitle= "Currently Reading"
-                  books={this.state.books.filter((book) => book.shelf === 'currentlyReading')}
+                  shelfTitle = "Currently Reading"
+                  books = {this.state.books.filter((book) => book.shelf === 'currentlyReading')}
                   onUpdateShelf = {this.updateShelf}
                 />
                 <ListBooks
-                  shelfTitle= "Want to Read"
-                  books={this.state.books.filter((book) => book.shelf === 'wantToRead')}
+                  shelfTitle = "Want to Read"
+                  books = {this.state.books.filter((book) => book.shelf === 'wantToRead')}
                   onUpdateShelf = {this.updateShelf}
                 />
                 <ListBooks
-                  shelfTitle= "Read"
-                  books={this.state.books.filter((book) => book.shelf === 'read')}
+                  shelfTitle = "Read"
+                  books = {this.state.books.filter((book) => book.shelf === 'read')}
                   onUpdateShelf = {this.updateShelf}
                 />
               </div>
             </div>
-            <div className="open-search">
-              <Link to="/search">Add a book</Link>
+            <div className = "open-search">
+              <Link to = "/search">Add a book</Link>
             </div>
           </div>
         )}/>
--- src/SearchBook.js
+++ src/SearchBook.js
@@ -1,4 +1,4 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import ListBooks from './ListBooks';
 import * as BooksAPI from './utils/BooksAPI';
@@ -17,32 +17,24 @@ class SearchBook extends Component {
 
   render() {
     return (
-      <div className="search-books">
-        <div className="search-books-bar">
-          <Link to="/" className="close-search" >Close</Link>
-          <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
+      <div className = "search-books">
+        <div className = "search-books-bar">
+          <Link to = "/" className="close-search" >Close</Link>
+          <div className = "search-books-input-wrapper">
             <input
-              type="text"
-              placeholder="Search by title or author"
-              value={this.props.query}
-              onChange={(event) => this.searchQuery(event.target.value)}
+              type = "text"
+              placeholder = "Search by title or author"
+              value = {this.props.query}
+              onChange = {(event) => this.searchQuery(event.target.value)}
             />
 
           </div>
         </div>
-        <div className="search-books-results">
-          <ol className="books-grid">
+        <div className = "search-books-results">
+          <ol className = "books-grid">
             <ListBooks
-              shelfTitle= ""
-              books={this.state.books}
+              shelfTitle = ""
+              books = {this.state.books}
               onUpdateShelf = {this.props.updateShelf}
             />
           </ol>
